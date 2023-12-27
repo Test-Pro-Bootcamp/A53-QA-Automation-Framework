3eddc4222c2653e2521502ca51d3fe73af8ec1f9
WchangQA <154923872+WchangQA@users.noreply.github.com>
2023-12-27 09:58:32 -0800

    //Registration test for Homework 16

---
+++ src/test/java/Homework16Test.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16Test extends BaseTest {
+    @Test
+    public void registrationNavigation() {
+
+//      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        driver.findElement(By.xpath("//a[@href='registration']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/registration");
+        driver.quit();
+    }
+}
