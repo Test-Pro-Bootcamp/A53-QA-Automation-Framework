9e3f5e9f2c2be9cebbcadf989874e931bc696703
RussB17 <ruslan.bohonosyuk@testpro.io>
2024-01-02 20:58:47 -0700

    Create HomeWork16Tests.java

---
+++ src/test/java/HomeWork16Tests.java
@@ -0,0 +1,31 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class HomeWork16Tests {
+
+    @Test
+
+    public void registrationNavigation() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        WebElement registrationForgotPassword = driver.findElement(By.cssSelector("a[href='registration']"));
+
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+}
