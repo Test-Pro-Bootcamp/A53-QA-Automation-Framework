6d231fe692c547d178eb3b8ebc2c77e2f0e2993a
Muhammad Fraz <muhammad@testPro.io>
2024-01-02 20:32:40 -0500

    Assignment1
    Assignment 1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -13,6 +13,7 @@ public class LoginTests extends BaseTest {
 //      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
