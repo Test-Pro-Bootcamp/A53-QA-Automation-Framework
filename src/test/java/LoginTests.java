5c6e3d54a55808bf285901df22be973ca080c244
Menderes Koc <mndrs.kc@gmail.com>
2024-01-03 20:39:40 -0600

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -22,4 +24,28 @@ public class LoginTests extends BaseTest {
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+}
+@Test
+public void loginValidEmailPassword(){
+
+    //PreCondition
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origin=*");
+
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    //Steps
+
+    String url ="https://qa.koel.app/";
+    driver.get(url);
+
+    //Email Field
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys("demo@class.com");
+   //Password Field
+
+
+
 }
