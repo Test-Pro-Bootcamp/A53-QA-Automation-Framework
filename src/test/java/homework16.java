8ce36b7770ac6b3c0d5ca3a02286727ddb617c14
Angelina0504 <nataliya.yusupov@testpro.io>
2024-01-05 15:07:47 -0700

    registration changes
    Registration and navigation changes, register with invalid password

---
+++ src/test/java/homework16.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class homework16 extends BaseTest {
+    @Test
+    public void navigateToRegistration() {
+
+        //Preconditions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        //Steps
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        //EmailField
+        WebElement emailField = driver.findElement(By.cssSelector ("input[type = 'email']"));
+        emailField.clear();
+        emailField.sendKeys("demo@class.com");
+        //passwordField
+        WebElement passwordField = driver.findElement(By.cssSelector ("input[type = 'password']"));
+        emailField.clear();
+        emailField.sendKeys("incpassword");
+        //Submit button
+        WebElement submitButton = driver.findElement(By.cssSelector ("button[type = 'submit']"));
+        submitButton.click();
+        //registrationField
+        WebElement registrationLink = driver.findElement(By.cssSelector ("[href='registration']"));
+        registrationLink.click();
+         String registrationUrl = "https://qa.koel.app/registration";
+
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
+        //quit
+        driver.quit();
+
+    }
+}
