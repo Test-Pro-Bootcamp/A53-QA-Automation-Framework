f210f898d45c6dbe946fe1c8e2f6315f78fb1f90
RussB17 <ruslan.bohonosyuk@testpro.io>
2024-01-06 16:53:07 -0700

    Made few changes but still not working, need help

--- src/test/java/HomeWork16Tests.java
+++ src/test/java/HomeWork16Tests.java
@@ -16,14 +16,15 @@ public class HomeWork16Tests {
     public void registrationNavigation() {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+       // options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String url = "https://qa.koel.app/";
         driver.get(url);
-        WebElement registrationNavigation = driver.findElement(By.cssSelector("a[href='registration']"));
+        WebElement registrationNavigation = driver.findElement(By.cssSelector("a[href*='registration']"));
+        registrationNavigation.click();
 
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
