9b6efb215681bd65e3f4871254fd49bf7dcda353
swhough <155691735+swhough@users.noreply.github.com>
2024-01-05 22:05:39 -0800

    .com change - LoginTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,7 +17,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://app.testpro.io";
+        String url = "https://app.testpro.com";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
