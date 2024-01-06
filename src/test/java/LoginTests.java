6598c09e47ca684e97e397f5b4022dbd227d99ec
Menderes Koc <mndrs.kc@gmail.com>
2024-01-06 15:50:49 -0600

    First commit

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,6 +17,7 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
