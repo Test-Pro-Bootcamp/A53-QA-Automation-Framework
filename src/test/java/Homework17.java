2df322af9b60fe8922ff337652e4798320ce394d
HedpeFan <angela.balderas@testpro.io>
2024-01-06 16:27:09 -0500

    created Profile Tests class created method helpers tested change profile name

---
+++ src/test/java/Homework17.java
@@ -0,0 +1,81 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework17 extends BaseTest {
+
+    @Test
+    public void addSongToPlaylist() throws InterruptedException {
+        String expectedSongAddedMessage = "Added 1 song into \"Postman HW.\"";
+
+        navigateToPage();
+        provideEmail("azahn007@gmail.com");
+        providePassword("Koelpass");
+        clickSubmit();
+        Thread.sleep(2000);
+        searchSong("BornKing");
+        clickViewAllBtn();
+        selectFirstSongResult();
+        clickAddToBtn();
+        choosePlaylist();
+        Assert.assertEquals(getAddSongToPlaylistSuccessMsg(), expectedSongAddedMessage);
+
+    }
+    public void searchSong(String name)  throws InterruptedException {
+        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+        searchField.sendKeys(name);
+        Thread.sleep(2000);
+    }
+    public void clickViewAllBtn() throws InterruptedException {
+        WebElement viewAll = driver.findElement(By.cssSelector(" [data-test='view-all-songs-btn']"));
+        viewAll.click();
+        Thread.sleep(2000);
+
+    }
+    public void selectFirstSongResult() throws InterruptedException {
+        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
+        firstSong.click();
+        Thread.sleep(2000);
+    }
+    public void clickAddToBtn() throws InterruptedException {
+        WebElement addToButton = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
+        addToButton.click();
+        Thread.sleep(2000);
+    }
+    public void choosePlaylist() throws InterruptedException {
+        WebElement playlist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[contains(text(),'Postman HW')]"));
+        playlist.click();
+        Thread.sleep(2000);
+    }
+    public String getAddSongToPlaylistSuccessMsg() {
+       WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+}
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
