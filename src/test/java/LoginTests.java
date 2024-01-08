b30fad5abf1ed47b1c7b5c6a93053b8997e48162
Menderes Koc <mndrs.kc@gmail.com>
2024-01-07 18:56:38 -0600

    Merge

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,7 +17,6 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-
         String url = "https://testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
