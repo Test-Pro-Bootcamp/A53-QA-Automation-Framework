0f250b4a72577414e2506bffab97c5e84b18afb9
Menderes Koc <mndrs.kc@gmail.com>
2024-01-07 21:23:52 -0600

    Updated test method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,60 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import java.time.Duration;
 
 public class BaseTest {
 
+    public WebDriver driver;
+
+    public String url = "https://qa.koel.app/";
+
     @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+    static void setupClass() {WebDriverManager.chromedriver().setup();}
+
+    @BeforeMethod
+    public void launchBrowser(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        //Manage Browser
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    @AfterMethod
+    public void closeBrowser(){
+        driver.quit();
+    }
+
+    void  provideEmail(){
+        //Email Field
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("menderes.koc@testpro.io");
+     }
+
+    void providePassword(){
+         //Password Field
+         WebElement passWordField = driver.findElement(By.cssSelector("input[type='password']"));
+         passWordField.clear();
+         passWordField.sendKeys("VGGd8tOG");
+     }
+
+    void clickSubmit() {
+        //Submit button
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+    }
+
+    //Helper Method
+    public void navigateToUrl(){
+        driver.get(url);
     }
 }
--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -4,9 +4,7 @@ import org.testng.annotations.Test;
 public class Homework17 extends BaseTest{
     @Test
     public void addSongToPlayList(){
-
-
-
+        navigateToUrl();
 
 
     }
