62d2ffcbb11f3fd58d78ae10f4c2962a3fc68017
czar-testpro <czar@testpro.io>
2024-01-09 20:03:41 +0800

    Added Lesson 19 class examples

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,35 +4,58 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
+
     public WebDriver driver = null;
-    public String url = "https://qa.koel.app/";
+    public String url = null;
+
+    @DataProvider(name="IncorrectLoginData")
+    public static Object[][] getDataFromDataProviders() {
+        return new Object[][] {
+                {"invalid@mail.com", "invalidPassword"},
+                {"demo@class.com", ""},
+                {"", ""}
+        };
+    }
+
+    //without static keyword
+//    @DataProvider(name="IncorrectLoginData")
+//    public Object[][] getDataFromDataProviders() {
+//        return new Object[][] {
+//                {"invalid@mail.com", "invalidPassword"},
+//                {"demo@class.com", ""},
+//                {"", ""}
+//        };
+//    }
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
     @BeforeMethod
-    public void launchBrowser() {
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL) {
         //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        url = BaseURL;
+        navigateToPage();
     }
+
     @AfterMethod
     public void closeBrowser() {
         driver.quit();
     }
-    public void navigateToPage() {
+    public  void navigateToPage() {
         driver.get(url);
     }
     public void provideEmail(String email) {
@@ -49,15 +72,6 @@ public class BaseTest {
         WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
         submit.click();
     }
-    public void clickAvatarIcon() {
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
-        avatarIcon.click();
-    }
-    public void provideCurrentPassword(String password) {
-        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
-        currentPassword.clear();
-        currentPassword.sendKeys(password);
-    }
     public void clickSaveButton() {
         WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
         saveButton.click();
@@ -67,11 +81,16 @@ public class BaseTest {
         profileName.clear();
         profileName.sendKeys(randomName);
     }
+    public void provideCurrentPassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
     public String generateRandomName() {
         return UUID.randomUUID().toString().replace("-", "");
     }
-    public void isAvatarDisplayed() {
-        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
-        Assert.assertTrue(avatarIcon.isDisplayed());
+    public void clickAvatarIcon() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
     }
 }
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,20 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+
 public class LoginTests extends BaseTest {
 
-    @Test (enabled = true, priority = 0, description = "Login with invalid email and valid password")
-    public void loginInvalidEmailValidPassword() throws InterruptedException {
+    @Test (dataProvider = "IncorrectLoginData", dataProviderClass = BaseTest.class, enabled = true, priority = 0, description = "Login with invalid email and valid password")
+    public void loginInvalidEmailValidPassword(String email, String password) throws InterruptedException {
 
-        navigateToPage();
-        provideEmail("invalid@class.com");
-        providePassword("te$t$tudent");
+        provideEmail(email);
+        providePassword(password);
         clickSubmit();
 
         Thread.sleep(2000);
-        Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
+        Assert.assertEquals(driver.getCurrentUrl(), url);
     }
 
+    @Test (dataProvider = "IncorrectLoginData", dataProviderClass = BaseTest.class)
+    public void loginEmptyEmailPassword(String email, String password) throws InterruptedException {
+        provideEmail(email);
+        providePassword(password);
+        clickSubmit();
+
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+    }
+    
     @Test (enabled = true, priority = 1, description = "Login with valid email and valid password")
     public void loginValidEmailPassword(){
 
@@ -36,5 +48,8 @@ public class LoginTests extends BaseTest {
         Thread.sleep(2000);
         Assert.assertEquals(driver.getCurrentUrl(), url); //https://qa.koel.app/
     }
-
+    public void isAvatarDisplayed() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
+    }
 }
