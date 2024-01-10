90e94d1ecee9ae9ea16039df9c06863274e60b1d
utku <mattaktas94@gmail.com>
2024-01-09 22:07:38 -0500

    Url given by Parameters
    deletePlaylsit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,35 +4,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
 
 import java.time.Duration;
 
 public class BaseTest {
+
     public WebDriver driver;
-    public String url = "https://qa.koel.app/";
 //Test
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
+
     @BeforeMethod
-    public void launchBrowser(){
+    @Parameters({"BaseURL"})
+    public void launchBrowser(String BaseURL){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         // Manage browser - wait for 10 seconds before failing/quitting
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        //Navigate to URL
-        navigateToUrl();
+
+        driver.get(BaseURL);
     }
 
+
     @AfterMethod
-    public void closeBrowser(){driver.quit();}
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(4000);
+        driver.quit();}
 
     void provideEmail(String email){
         WebElement emailField =  driver.findElement((By.cssSelector("input[type='email']")));
@@ -51,9 +58,31 @@ public class BaseTest {
         submitButton.click();
     }
 
-
-    public void navigateToUrl(){
-        driver.get(url);
+    void isLoggedIn(){
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
     }
+
+    void clickOnThePlaylistAndDelete(String playlistName) throws InterruptedException{
+        try{
+
+        WebElement playlist = driver.findElement(By.xpath("//div[@id='mainWrapper']//a[contains(text(), '"+playlistName+"')]"));
+        playlist.click();
+
+        WebElement deleteButton = driver.findElement(By.cssSelector("button.btn-delete-playlist"));
+        deleteButton.click();
+
+        WebElement okButton = driver.findElement(By.cssSelector("button.ok"));
+        okButton.click();
+
+        WebElement warningMessage = driver.findElement(By.cssSelector("div.success"));
+        Assert.assertTrue(warningMessage.isDisplayed());
+        Assert.assertEquals("Deleted playlist \"" +playlistName+".\"",warningMessage.getText());
+        }catch(Exception e){
+            System.out.println("Something went wrong!! -> " + e);
+        }
+    }
+
+
 }
 //Hello Test Commitments
