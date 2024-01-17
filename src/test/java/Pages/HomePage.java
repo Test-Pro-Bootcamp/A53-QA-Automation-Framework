37018956c5e033bd257c5a59b085fc242d51089b
simmons-andrew <154620524+simmons-andrew@users.noreply.github.com>
2024-01-17 10:21:07 -0500

    Homework 22

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -4,18 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class HomePage extends BasePage{
+public class HomePage extends BasePage {
 
-    public HomePage(WebDriver givenDriver){
+    public HomePage(WebDriver givenDriver) {
         super(givenDriver);
     }
 
     //Page Locators
     By userAvatarIcon = By.cssSelector("img.avatar");
 
+    By playButton = By.cssSelector("driver.findElement(By.cssSelector(\"span .play i\"));");
+
 
     //Page Methods
-    public WebElement getUserAvatarIcon(){
+    public WebElement getUserAvatarIcon() {
         return findElementUsingByLocator(userAvatarIcon);
     }
+
 }
