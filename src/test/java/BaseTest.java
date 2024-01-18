a9b26040f5a0f67f76f258ef582b4acd9c5b0450
Vladislav Sadokha <sadokha.david@gmail.com>
2024-01-18 00:32:43 -0800

    Homework18 (StepbyStep)
    Instructions followed from L19.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -3,9 +3,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
 
 public class BaseTest {
+    @parameters({"BaseUrl"})
     public String url = "https://qa.koel.app/";
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    protected void provideEmail() {
+        // stuff
+    }
+
+    protected void providePassword() {
+        // stuff
+    }
+
+    protected void clickSubmit() {
+        // stuff
+    }
+
+
 }
--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,11 +1,38 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-public class Homework18 {
+public class Homework18 extends BaseTest{
+
+    WebDriver driver = new ChromeDriver();
+    @Parameters({"BaseUrl"})
 
     @Test
-    public void PlaySong(){
-
+    public void playSong(){
+        provideEmail("vlad.sadokha@testpro.io");
+        providePassword("Mukmuk11");
+        clickSubmit();
+        //clickPlay();
+            //assertions
+        Assert.assertTrue(isSongPlaying());
     }
+
+    public void clickPlay(){
+        WebElement  playNextButton = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+
+        playNextButton.click();
+        playButton.click();
+    }
+    public boolean isSongPlaying() {
+        WebElement soundBar = driver.findElement(By.xpath("//div[@data-testid='sound-bar-play']"));
+        return soundBar.isDisplayed();
+    }
+
 }
