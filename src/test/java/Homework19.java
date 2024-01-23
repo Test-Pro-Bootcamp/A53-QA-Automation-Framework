8e732b8de741ab6010609bf91a12e704e53a0150
Djankoy <154765241+Djankoy@users.noreply.github.com>
2024-01-22 19:58:43 -0500

    homework_19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+
+        navigateToUrl(url);
+        provideEmail("dmitry.lobachev@testpro.io");
+        providePassword("Chebyreki5!");
+        clickSubmit();
+        Thread.sleep(2000);
+
+
+        //Select the Playlist Titled "Delete Me"
+        WebElement playlist = driver.findElement(By.cssSelector("#playlists ul li:nth-child(4)"));
+        playlist.click();
+
+        //Select the Delete Playlist Button
+        WebElement deleteButton = driver.findElement(By.cssSelector("button[class='del btn-delete-playlist']"));
+        deleteButton.click();
+
+
+        Assert.assertEquals(getDeletedPlaylistMsg(), "Deleted playlist \"Delete Me!.\"");
+
+
+    }
+
+    public String getDeletedPlaylistMsg(){
+        WebElement deleteNotification = driver.findElement(By.cssSelector(".success.show"));
+        return deleteNotification.getText();
+    }
+
+}
+
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,28 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
-
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
+    public void navigateToKoelApp() {
+        navigateToUrl();
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+
+    @Test
+    public void loginValidEmailPassword() {
+        try {
+            provideEmail("dmitry.lobachev@testpro.io");
+            providePassword("Chebyreki5!");
+            clickSubmit();
+            //Assertion
+            WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+
+            Assert.assertTrue(avatarIcon.isDisplayed());
+        } catch (Exception e) {
+            System.out.println("Something went wrong." + e);
+            Assert.fail("Something went wrong." + e);
+        }
     }
 }
