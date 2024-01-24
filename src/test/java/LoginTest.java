3912de92ef0f07ce42c4693b9da6238b21babda7
utku <mattaktas94@gmail.com>
2024-01-24 10:26:00 -0500

    invisibilityOfElementLocated feature added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,8 +1,14 @@
 import Pages.HomePage;
 import Pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTest extends BaseTest{
 
@@ -27,13 +33,18 @@ public class LoginTest extends BaseTest{
         loginPage.providePassword("AAekga9uf6");
         loginPage.clickSubmit();
 
+//        Assert.assertTrue(loginPage.isLoginBtnVisible()); // Login Button Approach
 
-//        Assert.assertFalse(homePage.getUserAvatarIcon()); // Didn't work
-        Assert.assertTrue(loginPage.isLoginBtnVisible()); // Didn't work
+        boolean isAvatarNotVisible = waitForInvisibilityOfElement(By.cssSelector("img.avatar"), 2);
+        Assert.assertTrue(isAvatarNotVisible,"User avatar icon should not be displayed after incorrect login");
+    }
 
-
-//        Assert.assertTrue(!homePage.getUserAvatarIcon().isDisplayed()); //Didn't work
-//        Assert.assertTrue(homePage.getUserAvatarIcon().isDisplayed()); //Didn't work
-
+    private boolean waitForInvisibilityOfElement(By locator, int timeoutInSeconds){
+        try{
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(500));
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+        }catch (Exception e){
+            return false;
+        }
     }
 }
