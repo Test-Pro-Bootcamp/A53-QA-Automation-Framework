40e9c99454433a3f76074eec4f29ae67ace36291
utku <mattaktas94@gmail.com>
2024-01-27 09:40:03 -0500

    Complated

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -15,6 +18,8 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -24,7 +29,9 @@ public class BaseTest {
 //Test
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.chromedriver().setup();
+//        WebDriverManager.firefoxdriver().setup();
+
     }
     @BeforeMethod
     @Parameters({"BaseURL"})
@@ -44,4 +51,36 @@ public class BaseTest {
     public void closeBrowser() {
         driver.quit();
     }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException{
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.162:4444/";
+        switch(browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(chromeOptions);
+
+            case "grid-firefox": // gradle clean test -Dbrowser=grid-firefox
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            case "grid-chrome": // gradle clean test -Dbrowser=grid-chrome
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptionsDefault = new ChromeOptions();
+                chromeOptionsDefault.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(chromeOptionsDefault);
+        }
+    }
 }
+//Selenium Grid
+//Running Commnet
+//java -jar selenium-server-4.16.1.jar standalone
