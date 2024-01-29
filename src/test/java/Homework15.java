3bc0bd58c894d90e60f8a03e65c4194130a93f8d
Vladislav Sadokha <sadokha.david@gmail.com>
2024-01-28 17:43:56 -0800

    Halp

---
+++ src/test/java/Homework15.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework15 {
+    @Test
+    public void openBrowser() {
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://testpro.io/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+}
