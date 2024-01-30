648c03aef46e0a0ffdf18b68f53b43073f75b170
Djankoy <154765241+Djankoy@users.noreply.github.com>
2024-01-30 17:02:18 -0500

    Summary
    Issue before login

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,18 +1,13 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-public class Homework17 extends BaseTest  {
+public class Homework17 extends BaseTest {
 
     @Test
-    public void addSongToPlaylist() throws InterruptedException{
+    public void addSongToPlaylist() throws InterruptedException {
 
-        driver.get(url);
         //Login
         //Email Field
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
@@ -48,30 +43,25 @@ public class Homework17 extends BaseTest  {
         //Verify
         WebElement successAlert = driver.findElement(By.cssSelector(".alertify-logs .success"));
         String correctAlert = "Added 1 song into \"AutoTest.\"";
-        Assert.assertEquals(successAlert.getText(), correctAlert );
+        Assert.assertEquals(successAlert.getText(), correctAlert);
 
-        //Delete song from playlist so it can be tested again.
-        /*WebElement addedPlaylist = driver.findElement(By.xpath("//*[@id='sidebar']//*[contains(text(), \"AutoTest\")]"));
-        addedPlaylist.click();
-        WebElement addedSong = driver.findElement(By.xpath)*/
-        driver.quit();
     }
 
     //Helper Methods
 
-    public void clickViewAll(){
+    public void clickViewAll() {
         WebElement viewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
         viewAllButton.click();
     }
 
     //Click on the first Song on the Page
-    public void clickSong(){
+    public void clickSong() {
         WebElement firstSong = driver.findElement(By.cssSelector(".search-results .items tr:nth-child(1)"));
         firstSong.click();
     }
 
     //Add Song to playlist
-    public void addToPlaylist(){
+    public void addToPlaylist() {
         WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to"));
         WebElement addToPlaylist = driver.findElement(By.cssSelector("#songResultsWrapper .existing-playlists  li:nth-child(5)"));
         addToButton.click();
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -17,3 +17,9 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
+    }
+}
+
+
+
+
