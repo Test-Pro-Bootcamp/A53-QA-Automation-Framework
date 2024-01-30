f743d520ab1c103c2b6aa512764131c249f60514
vlad199213 <vladkashchuk@gmail.com>
2024-01-29 18:28:59 -0600

    1

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends BasePage {
+    public LoginPage (WebDriver givenDriver) {
+        super(givenDriver);
+    }
+}
