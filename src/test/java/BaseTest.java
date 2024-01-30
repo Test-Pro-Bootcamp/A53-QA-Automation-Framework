50d58d14e7a708578c54a08f8cd0630aa377ebc0
Djankoy <154765241+Djankoy@users.noreply.github.com>
2024-01-30 12:05:59 -0500

    Summary
    Tried to start from the beginning

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,147 +1,88 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.FluentWait;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.UUID;
 
 public class BaseTest {
-
-    //Data Providers
-    @DataProvider(name="InvalidLoginData")
-    public static Object[][] getDataFromDataProviders(){
-        return new Object[][] {
-                {"invalid@mail.com", "invalidPassword"},
-                {"demo@class.com", ""},
-                {"", "te$t$tudent"},
-                {"",""}
-        };
-    }
-
-    public WebDriver driver;
-    public WebDriverWait wait;
-
-    public Wait<WebDriver> fluentWait;
-
+    public WebDriver driver = null;
     public String url = "https://qa.koel.app/";
-
-    public Actions actions;
-
     @BeforeSuite
-    static void setupClass() {
-        //WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
+    void setupClass() {
+        WebDriverManager.chromedriver().setup();
     }
-
-    @Parameters({"BaseUrl"})
     @BeforeMethod
-    public void launchBrowser(String BaseUrl) throws MalformedURLException {
-        //Chrome Options
-        /*ChromeOptions options = new ChromeOptions();
+    public void launchBrowser() {
+        //      Added ChromeOptions argument below to fix websocket error
+        ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        //Manage Browser - wait for 10 seconds before failing/quitting.
-        driver = new ChromeDriver(options);*/
-        //driver = new FirefoxDriver();
-        driver = pickBrowser(System.getProperty("browser"));
-        //Implicit Wait
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        //Explicit Wait
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //Fluent Wait
-        fluentWait = new FluentWait<WebDriver>(driver)
-                .withTimeout(Duration.ofSeconds(10))
-                .pollingEvery(Duration.ofSeconds(5))
-                .ignoring(NoSuchElementException.class);
-                //.ignoring(NoAlertPresentException.class);
-        actions = new Actions(driver);
-        driver.manage().window().maximize();
-        //String url = BaseUrl;
-        //Navigate to Url
-        navigateToUrl(BaseUrl);
-    }
 
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
     @AfterMethod
-    public void closeBrowser(){
+    public void closeBrowser() {
         driver.quit();
     }
 
-    void provideEmail(String email) {
-        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
-        //WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+
+    public void navigateToPage() {
+        driver.get(url);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
         emailField.sendKeys(email);
     }
 
-    void providePassword(String password) {
-        //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
-    void clickSubmit(){
-        //WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
-        WebElement submitButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
-        submitButton.click();
+    public void clickSubmit() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+    public void isAvatarDisplayed() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
+        Assert.assertTrue(avatarIcon.isDisplayed());
     }
 
-    //Helper Method
-    public void navigateToUrl(String givenUrl){
-        driver.get(givenUrl);
+    public void clickSaveButton() {
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        saveButton.click();
     }
 
-    public void loginToKoelApp(){
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
+    public void provideProfileName(String randomName) {
+        WebElement profileName = driver.findElement(By.cssSelector("[name='name']"));
+        profileName.clear();
+        profileName.sendKeys(randomName);
     }
 
-    public WebDriver pickBrowser(String browser) throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        String gridURL = "http://10.0.0.206:4444/";
-        switch (browser){
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                return driver = new FirefoxDriver();
-
-            case "MicrosoftEdge":
-                WebDriverManager.edgedriver().setup();
-                EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.addArguments("--remote-allow-origins=*");
-                return driver = new EdgeDriver();
-
-            case "grid-edge": // gradle clean test -Dbrowser=grid-edge
-                caps.setCapability("browserName", "MicrosoftEdge");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            case "grid-firefox": // gradle clean test -Dbrowser=grid-firefox
-                caps.setCapability("browserName", "firefox");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            case "grid-chrome": // gradle clean test -Dbrowser=grid-chrome
-                caps.setCapability("browserName", "chrome");
-                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
-
-            default:
-                WebDriverManager.chromedriver().setup();
-                ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.addArguments("--remote-allow-origins=*");
-                return driver = new ChromeDriver(chromeOptions);
-        }
+    public void provideCurrentPassword(String password) {
+        WebElement currentPassword = driver.findElement(By.cssSelector("[name='current_password']"));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
     }
 
+    public String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public void clickAvatarIcon() {
+        WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+        avatarIcon.click();
+    }
 }
---
+++ src/test/java/Homework17.java
@@ -0,0 +1,81 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+public class Homework17 extends BaseTest  {
+
+    @Test
+    public void addSongToPlaylist() throws InterruptedException{
+
+        driver.get(url);
+        //Login
+        //Email Field
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys("dmitry.lobachevs@testpro.io");
+        //Password Field
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("Chebyreki5!");
+        //Submit button
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        submitButton.click();
+
+        Thread.sleep(2000);
+
+
+        //Click on the search bar and search for a song
+        WebElement searchBar = driver.findElement(By.cssSelector("input[type='search']"));
+        searchBar.clear();
+        searchBar.sendKeys("Dark Days");
+
+        //Find and Click on the View All Button
+        clickViewAll();
+
+        //Click the first song on the page
+        clickSong();
+
+        //Add the song to a playlist
+        addToPlaylist();
+
+        Thread.sleep(500);
+
+        //Verify
+        WebElement successAlert = driver.findElement(By.cssSelector(".alertify-logs .success"));
+        String correctAlert = "Added 1 song into \"AutoTest.\"";
+        Assert.assertEquals(successAlert.getText(), correctAlert );
+
+        //Delete song from playlist so it can be tested again.
+        /*WebElement addedPlaylist = driver.findElement(By.xpath("//*[@id='sidebar']//*[contains(text(), \"AutoTest\")]"));
+        addedPlaylist.click();
+        WebElement addedSong = driver.findElement(By.xpath)*/
+        driver.quit();
+    }
+
+    //Helper Methods
+
+    public void clickViewAll(){
+        WebElement viewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        viewAllButton.click();
+    }
+
+    //Click on the first Song on the Page
+    public void clickSong(){
+        WebElement firstSong = driver.findElement(By.cssSelector(".search-results .items tr:nth-child(1)"));
+        firstSong.click();
+    }
+
+    //Add Song to playlist
+    public void addToPlaylist(){
+        WebElement addToButton = driver.findElement(By.cssSelector("button.btn-add-to"));
+        WebElement addToPlaylist = driver.findElement(By.cssSelector("#songResultsWrapper .existing-playlists  li:nth-child(5)"));
+        addToButton.click();
+        addToPlaylist.click();
+    }
+
+}
