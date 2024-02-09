207c96dd47e9bc7c1034ec717682227990fa4222
Djankoy <154765241+Djankoy@users.noreply.github.com>
2024-02-08 23:54:01 -0500

    First step

---
+++ src/test/java/Homework21.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class Homework21 extends BaseTest{
+
+    //Prerequisite - at least one user-created playlist
+    String newPlaylistName = "Rename Playlist";
+
+    @Test
+    public void renamePlaylist(){
+
+        String updatedPlaylistMsg = "Updated playlist \"Renamed Playlist.\"";
+
+        provideEmail("dmitry.lobachev@testpro.io");
+        providePassword("Chebyreki5!");
+        clickSubmit();
+        doubleClickPlaylist();
+        enterNewPlaylistName();
+        Assert.assertEquals(getRenamePlaylistSuccessMsg(), updatedPlaylistMsg);
+
+
+
+    }
+    public void doubleClickPlaylist() {
+        WebElement playlistElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".playlist:nth-child(3)")));
+        actions.doubleClick(playlistElement).perform();
+    }
+    public void enterNewPlaylistName() {
+        WebElement playlistInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+//       clear() does not work, element has an attribute of "required"
+//       workaround is ctrl A (to select all) then backspace to clear then replace with new playlist name
+        playlistInputField.sendKeys(Keys.chord(Keys.CONTROL,"A", Keys.BACK_SPACE));
+
+        playlistInputField.sendKeys(newPlaylistName);
+        playlistInputField.sendKeys(Keys.ENTER);
+
+    }
+    public String getRenamePlaylistSuccessMsg() {
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        return notification.getText();
+    }
+}
