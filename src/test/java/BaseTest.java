e8de9c77fa6de9c4ff04e94ecf0bd09d8f02c6e5
Vladislav Sadokha <sadokha.david@gmail.com>
2024-02-13 21:35:37 -0800

    Updated?

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,29 +1,34 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
-
 import java.time.Duration;
 
-public WebDriver driver;
 
 public class BaseTest {
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    wait = new WebDriver(driver, 10);
+    driver = new ChromeDriver;
+    @Parameters({"BaseURL"})
+    @BeforeMethod
+    public void launchBrowser(String BaseURL) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+        driver.manage().window().maximize();
+    }
 }
 
-@Parameters({"BaseURL"})
-@BeforeMethod
-public void launchBrowser(String BaseURL) {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--remote-allow-origins=*");
-    driver = new ChromeDriver(options);
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
-    driver.manage().window().maximize();
-    //Navigate to the URL somehow.
-}
--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -4,58 +4,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class Homework19 {
-
-    private WebDriver driver;
-
-    @BeforeClass
-    public void setUp() {
-        // Set up WebDriver and other configurations
-    }
-
+public class Homework19 extends BaseTest {
     @Test
     @Parameters("baseUrl")
     public void deletePlaylist(String baseUrl) {
-        // Navigate to the specified URL
-        driver.get(baseUrl);
 
-        // Log in with credentials (use helper method)
-        login("yourUsername", "yourPassword");
-
-        // Click the playlist you want to delete or create a new one
-        WebElement playlistElement = getPlaylistElement("PlaylistName");
-        playlistElement.click();
-
-        // Locate and click the delete button
-        WebElement deleteButton = driver.findElement(By.xpath("//button[contains(text(), 'x PLAYLIST')]"));
-        deleteButton.click();
-
-        // Verify the confirmation notification text
-        String confirmationText = driver.findElement(By.id("confirmationMessage")).getText();
-        assert confirmationText.contains("Deleted playlist");
-
-        // Add more assertions or verifications as needed
-
-        // Call the helper method to delete the playlist
-
+        login();
+        navigateToPlaylist();
+        deletePlaylist();
+        Assert.assertEquals();
     }
 
-    @AfterClass
-    public void tearDown() {
-        // Clean up resources, close the browser, etc.
+    public void login() throws InterruptedException {
+        WebElement emailButton = driver.findElement(By.cssSelector(""));
+        emailButton.sendKeys("vlad.sadokha@testpro.io");
+        emailButton.click();
+
+        WebElement passwordButton = driver.findElement(By.cssSelector(""));
+        passwordButton.sendKeys("democlass");
+        passwordButton.click();
+
+        WebElement submitButton = driver.findElement(By.cssSelector(""));
+        submitButton.click();
+        Thread.sleep(2000);
     }
 
-    // Define helper methods
-    private void login(String username, String password) {
-        // Implement login functionality
+    public void navigateToPlaylist() throws InterruptedException {
+        WebElement goPlaylist = driver.findElement(By.ByCssSelector(""));
+        goPlaylist.click();
     }
 
-    private WebElement getPlaylistElement(String playlistName) {
-        // Implement logic to find and return the playlist element
-    }
+
 }
