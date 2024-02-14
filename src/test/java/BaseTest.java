b52504c94abb9484c7c6a4030a57f1c06325c2cc
Vladislav Sadokha <sadokha.david@gmail.com>
2024-02-13 23:24:46 -0800

    done

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,34 +1,88 @@
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.UUID;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.*;
+
 import java.time.Duration;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 
 public class BaseTest {
+
+    public WebDriver driver = null;
+    public String url = null;
+    public WebDriverWait wait = null;
+
+
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
 
-    protected WebDriver driver;
-    protected WebDriverWait wait;
-
-    wait = new WebDriver(driver, 10);
-    driver = new ChromeDriver;
-    @Parameters({"BaseURL"})
     @BeforeMethod
+    @Parameters({"BaseURL"})
     public void launchBrowser(String BaseURL) {
+        // Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+
         driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        url = BaseURL;
+        navigateToPage();
+    }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+    public  void navigateToPage() {
+        driver.get(url);
+    }
+    public void provideEmail(String email) {
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='email']")));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+    public void providePassword(String password) {
+        WebElement passwordField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[type='password']")));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public void clickSubmit() {
+        WebElement submit = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button[type='submit']")));
+        submit.click();
+    }
+    public void clickSaveButton() {
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.btn-submit")));
+        saveButton.click();
+    }
+    public void provideProfileName(String randomName) {
+        WebElement profileName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='name']")));
+        profileName.clear();
+        profileName.sendKeys(randomName);
+    }
+    public void provideCurrentPassword(String password) {
+        WebElement currentPassword = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("[name='current_password']")));
+        currentPassword.clear();
+        currentPassword.sendKeys(password);
+    }
+    public String generateRandomName() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+    public void clickAvatarIcon() {
+        WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("img.avatar")));
+        avatarIcon.click();
     }
 }
+
 
--- src/test/java/Homework19.java
+++ src/test/java/Homework19.java
@@ -11,33 +11,31 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Homework19 extends BaseTest {
-    @Test
-    @Parameters("baseUrl")
-    public void deletePlaylist(String baseUrl) {
 
-        login();
-        navigateToPlaylist();
-        deletePlaylist();
-        Assert.assertEquals();
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        String expectedPlaylistDeleteMessage = "Deleted playlist / 'TestPro PlayList./";
+
+        provideEmail("vlad.sadokha@testpro.io");
+        providePassword("democlass");
+        clickSubmit();
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        Assert.assertEquals(getDeletedPlaylistMsg(), expectedPlaylistDeleteMessage);
     }
 
-    public void login() throws InterruptedException {
-        WebElement emailButton = driver.findElement(By.cssSelector(""));
-        emailButton.sendKeys("vlad.sadokha@testpro.io");
-        emailButton.click();
-
-        WebElement passwordButton = driver.findElement(By.cssSelector(""));
-        passwordButton.sendKeys("democlass");
-        passwordButton.click();
-
-        WebElement submitButton = driver.findElement(By.cssSelector(""));
-        submitButton.click();
+    public void openPlaylist() {
+        WebElement emptyPlaylist = driver.findElement(By.cssSelector(".playlist:nth-child(3)"));
+        emptyPlaylist.click();
+    }
+    public void clickDeletePlaylistBtn() throws InterruptedException {
+        WebElement deletePlaylist = driver.findElement(By.ByCssSelector("#playlistWrapper div.song-list-controls"));
+        deletePlaylist.click();
         Thread.sleep(2000);
     }
-
-    public void navigateToPlaylist() throws InterruptedException {
-        WebElement goPlaylist = driver.findElement(By.ByCssSelector(""));
-        goPlaylist.click();
+    public void getDeletedPlaylistMsg() {
+        WebElement notificationMsg = driver.findElement(By.ByCssSelector("div.alertify-logs.top.right"));
+        return notificationMsg.getText();
     }
 
 
