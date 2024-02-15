b4143bd0a49ce0f78f7c76c5e26fbfd6aca97d13
Vladislav Sadokha <sadokha.david@gmail.com>
2024-02-15 10:15:19 -0800

    UpdateLoginPage

--- src/test/java/P/O/M/LoginPage.java
+++ src/test/java/P/O/M/LoginPage.java
@@ -17,6 +17,7 @@ public class LoginPage extends BasePage {
 
     //Page Methods
 
+    //Author - Vladislav Sadokha
     public void provideEmail(String email) {
         findElementUsingByLocator(emailField).sendKeys(email);
     }
