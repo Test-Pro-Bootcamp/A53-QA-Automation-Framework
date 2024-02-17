9b32616563454a185b840bf5f09ff8d499476049
Vladislav Sadokha <sadokha.david@gmail.com>
2024-02-17 12:45:59 -0800

    Homework15
    Update

--- src/test/java/Homework15.java
+++ src/test/java/Homework15.java
@@ -1,18 +1,31 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class Homework15 {
+public class Homework15 extends BaseTest {
+
     @Test
     public void openBrowser() {
+        // Instantiate ChromeOptions and WebDriver
+        ChromeOptions options = new ChromeOptions();
         WebDriver driver = new ChromeDriver(options);
+
+        // Set implicit wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // Navigate to the specified URL
         String url = "https://koel.app/";
         driver.get(url);
+
+        // Assert that the current URL is as expected
         Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        // Close the browser
         driver.quit();
     }
 }
+
